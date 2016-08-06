02a6a09ac5a41985dd6c452868d5f65d0a4bb9a3
jotase <jota.segovia@gmail.com>
2016-08-06 01:10:43 -0400

    module spec

--- spec/undecided_spec.rb
+++ spec/undecided_spec.rb
@@ -1,11 +1,8 @@
 require 'spec_helper'
 
 describe Undecided do
+  let(:validator) { Undecided::Validator.new }
   it 'has a version number' do
     expect(Undecided::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
   end
 end
