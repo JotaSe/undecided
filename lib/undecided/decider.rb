364fd1e2c6f48a016d36032248558da5fdf8c6b9
jotase <jota.segovia@gmail.com>
2016-08-07 20:30:24 -0400

    * abstract methods that no compete with responsability

--- lib/undecided/decider.rb
+++ lib/undecided/decider.rb
@@ -1,42 +1,21 @@
 module Undecided
+  class DeciderError < StandardError
+  end
   # Evaluate boolean expression
   class Decider
-    def initialize
-      @evaluator = Undecided::Evaluator.new
-    end
-
     # Given a boolean expression and data to replace, return result
     def decide(rule, values)
-      @rule = rule
-      @values = values
-      raise 'Wrong data to perform' unless @evaluator.valid?(rule, values)
+      error unless Undecided::Evaluator.valid?(rule, values)
       # Sanitize data
-      sanitize
       # Eval rules and values after process it, with safe data
-      eval rule
+      final_expression = Converter.replacing_variables(rule, values)
+      eval final_expression
+    rescue
+      error
     end
 
-    # Sanitize params to transform data to a standar way
-    def sanitize
-      # Transform every keys to symbol
-      @values = Converter.to_symbol @values
-      # Transform every value to a boolean type
-      @values = values_to_bool
-      # Replace every variable from rules with values
-      replacing_variables
-    end
-
-    # Convert every value from hash to a boolean value
-    def values_to_bool
-      raise 'Only hash values' unless @values.is_a? Hash
-      @values.map { |k, v| [k, Converter.to_bool(v)] }.to_h
-    end
-
-    # Replaces variables from rules with values
-    def replacing_variables
-      @values.each do |k, v|
-        @rule.gsub! k.to_s, v.to_s
-      end
+    def error
+      raise DeciderError, 'Wrong data to perform'
     end
   end
 end
