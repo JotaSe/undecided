de53c29f7701c81187869518583b758f093b58de
jotase <jota.segovia@gmail.com>
2016-08-07 20:24:48 -0400

    * added methods hash_values_to_bool, replaces_variables, and process_values'  * fix typo

--- lib/undecided/converter.rb
+++ lib/undecided/converter.rb
@@ -1,5 +1,10 @@
 # Convert data to required type
 class Converter
+  def self.process_values(values)
+    values = to_symbol(values)
+    hash_values_to_bool(values)
+  end
+
   # Convert array, hash's keys or solo values to symbol
   def self.to_symbol(value)
     if value.is_a?(Array)
@@ -12,14 +17,28 @@ class Converter
     end
   end
 
+  # Convert every value from hash to a boolean value
+  def self.hash_values_to_bool(values)
+    raise 'Only hash values' unless values.is_a? Hash
+    values.map { |k, v| [k, to_bool(v)] }.to_h
+  end
+
   # Transform value to boolean
   def self.to_bool(value)
     # if boolean return value
-    return value if @evaluator.bool?(value)
+    return value if Undecided::Evaluator.bool?(value)
     # transform integer value to boolean
     !value.zero?
   rescue
     raise "#{value} Is not a correct value, " \
           'insert (1 or 0) or (true or false)'
+  end
+
+  # Replaces variables from rules with values
+  def self.replacing_variables(rule, values)
+    process_values(values).each do |k, v|
+      rule.gsub! k.to_s, v.to_s
+    end
+    rule
   end
 end
